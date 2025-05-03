bdc5560f5c2928f20e3b69535f7ebf7509bcc64f
dems7 <dembam136@gmail.com>
2025-05-03 19:28:18 -0400

    Initial commit - Spring Boot BookLoan project

---
+++ src/main/java/com/example/bookloan/BookloanApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookloanApplication.class, args);
+	}
+
+}
